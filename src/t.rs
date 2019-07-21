18fc2ff480c8ccff029f060745418d7d0726fb26
Konstantin Kostov <konstantin@headbright.be>
2019-07-21 18:34:21 +0200

    Add t::load which can take a string path

--- src/t.rs
+++ src/t.rs
@@ -3,7 +3,7 @@
 use crate::store::{Store, Unit};
 use std::fs::File;
 use std::io::Read;
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 
 /// Translation helper
 pub struct T {
@@ -11,8 +11,26 @@ pub struct T {
 }
 
 impl T {
+    /// Reads and interprets the contents of the specified file.
+    ///
+    /// # Errors
+    /// This function will return an error if reading the file is not successful.
+    pub fn load<P: AsRef<Path>>(path: P) -> Self {
+        let file_path = path.as_ref();
+        let mut file = File::open(file_path).unwrap();
+
+        let mut buffer: Vec<u8> = Default::default();
+        file.read_to_end(&mut buffer).expect("failed to read file");
+
+        let mut store: Store = Store::new();
+        store.load(buffer.iter().as_slice());
+
+        return T { store };
+    }
+
     /// Initializes a repository for the specified translation format.
-    pub fn load(path: &PathBuf) -> Self {
+    #[deprecated(since = "0.1.2", note = "please use `load<P: AsRef<Path>>(path: P)` instead")]
+    pub fn load_(path: &PathBuf) -> Self {
         let mut file = File::open(path).expect("Failed to open the file");
 
         let mut buffer: Vec<u8> = Default::default();
